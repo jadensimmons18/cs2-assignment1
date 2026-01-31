bda7f9ba82cf67e3ecacc3f5a239c260a70155f2
Jaden Simmons <jadensimmons1803@gmail.com>
2026-01-30 20:14:35 -0500

    Made styling changes

--- SensorRange.java
+++ SensorRange.java
@@ -6,21 +6,26 @@
 
 import java.util.Arrays;
 
-public class SensorRange {
+public class SensorRange 
+{
     
     // Brute force approach that returns the query results in O(nq)
-    public static int[] rangeCountBF(int[] readings, int[][] queries){
+    public static int[] rangeCountBF(int[] readings, int[][] queries)
+    {
         int numQueries = queries.length;
         int[] results = new int[numQueries];
         int queryResult = 0;
         int queryResultsIndex = 0;
 
-        for (int i = 0; i < numQueries; i++){
+        for (int i = 0; i < numQueries; i++)
+        {
             int L = queries[i][0];
             int H = queries[i][1];
 
-            for (int j = 0; j < readings.length; j++){
-                if (readings[j] <= H && readings[j] >= L){
+            for (int j = 0; j < readings.length; j++)
+            {
+                if (readings[j] <= H && readings[j] >= L)
+                {
                     queryResult++;
                 }
             }
@@ -33,16 +38,20 @@ public class SensorRange {
     }
 
     // Uses binary search to find L in O(logn)
-    static int findL(int[] arr, int target) {
+    static int findL(int[] arr, int target) 
+    {
         int left = 0;
         int right = arr.length;
 
-        while (left < right) {
+        while (left < right) 
+        {
             int mid = (left + right) / 2;
 
-            if (arr[mid] < target) {
+            if (arr[mid] < target) 
+            {
                 left = mid + 1;  
-            } else {
+            } else 
+            {
                 right = mid;
             }
         }
@@ -51,16 +60,20 @@ public class SensorRange {
     }
 
     // Finds the index of H in O(logn)
-    static int findH(int[] arr, int target) {
+    static int findH(int[] arr, int target) 
+    {
         int left = 0;
         int right = arr.length;
 
-        while (left < right) {
+        while (left < right) 
+        {
             int mid = (left + right) / 2;
 
-            if (arr[mid] <= target) {
+            if (arr[mid] <= target) 
+            {
                 left = mid + 1;  
-            } else {
+            } else 
+            {
                 right = mid;
             }
         }
@@ -69,7 +82,8 @@ public class SensorRange {
     }
 
     // Returns query result array in O(nlogn + qlogn)
-    public static int[] rangeCountFast(int[] readings, int[][] queries){
+    public static int[] rangeCountFast(int[] readings, int[][] queries)
+    {
         // Sort the readings in O(nlogn)
         Arrays.sort(readings);
 
@@ -77,7 +91,8 @@ public class SensorRange {
         int arrIndex = 0;
 
         // loops through queries and returns the result for each query into an array in O(qlogn)
-        for (int i = 0; i < queries.length; i++){
+        for (int i = 0; i < queries.length; i++)
+        {
             int L = findL(readings, queries[i][0]);
             int H = findH(readings, queries[i][1]);
             int result = H - L;
