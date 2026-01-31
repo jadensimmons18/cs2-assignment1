62ac2a307d4cf58adc2593d24e757db6877246f2
Jaden Simmons <jadensimmons1803@gmail.com>
2026-01-30 20:03:10 -0500

    added some comments

--- SensorRange.java
+++ SensorRange.java
@@ -7,7 +7,8 @@
 import java.util.Arrays;
 
 public class SensorRange {
-
+    
+    // Brute force approach that returns the query results in O(nq)
     public static int[] rangeCountBF(int[] readings, int[][] queries){
         int numQueries = queries.length;
         int[] results = new int[numQueries];
@@ -49,7 +50,7 @@ public class SensorRange {
         return left;
     }
 
-
+    // Finds the index of H in O(logn)
     static int findH(int[] arr, int target) {
         int left = 0;
         int right = arr.length;
@@ -67,6 +68,7 @@ public class SensorRange {
         return left;
     }
 
+    // Returns query result array in O(nlogn + qlogn)
     public static int[] rangeCountFast(int[] readings, int[][] queries){
         // Sort the readings in O(nlogn)
         Arrays.sort(readings);
@@ -74,6 +76,7 @@ public class SensorRange {
         int[] results = new int[queries.length];
         int arrIndex = 0;
 
+        // loops through queries and returns the result for each query into an array in O(qlogn)
         for (int i = 0; i < queries.length; i++){
             int L = findL(readings, queries[i][0]);
             int H = findH(readings, queries[i][1]);
